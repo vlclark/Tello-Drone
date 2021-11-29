e30f4fd664ba9e442543a8c5c074ae655a1c3d0b
Vance Clark <02vance.clark@gulllakecs.org>
2021-11-29 09:40:08 -0800

    Competition File Updated

--- Competition.py
+++ Competition.py
@@ -43,10 +43,27 @@ recvThread.start()
 
 # CREATE FUNCTIONS HERE....
 
+def firstHoop():
 
-print("\nFirst & Last Names")
-print("Program Name: ")
-print("Date: ")
+
+
+'''
+# def secondHoop():
+
+
+
+
+
+# def thirdHoop():
+
+
+
+
+# def fourthHoop():
+'''
+print("\nTeam: Clark and Kohler")
+print("Program Name: Hoop Competition")
+print("Date: 11.29.2021")
 print("\n****CHECK YOUR TELLO WIFI ADDRESS****")
 print("\n****CHECK SURROUNDING AREA BEFORE FLIGHT****")
 ready = input('\nAre you ready to take flight: ')
